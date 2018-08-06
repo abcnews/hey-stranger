9d683ef3e8f2616c2d708deee6fc4750e2d5329f
Colin Gourlay <Gourlay.Colin@abc.net.au>
2018-08-06 10:10:40 +1000

    RingNav component is functional

--- src/components/RingNav/index.js
+++ src/components/RingNav/index.js
@@ -1,42 +1,19 @@
-const { h, Component } = require('preact');
+const { h } = require('preact');
 const Arrow = require('../Arrow');
 const Button = require('../Button');
 const styles = require('./styles.css');
 
-class RingNav extends Component {
-  constructor(props) {
-    super(props);
+module.exports = ({ prev, next, isUnavailable, navigate }) => (
+  <nav className={styles.root} aria-hidden={isUnavailable ? 'true' : 'false'}>
+    <Button tabindex={isUnavailable ? -1 : 0} aria-controls="reader-stories" onClick={() => navigate(prev)}>
+      <Arrow direction="left" hasTail />
+      <span>Previous</span>
+    </Button>
+    <Button tabindex={isUnavailable ? -1 : 0} aria-controls="reader-stories" onClick={() => navigate(next)}>
+      <span>Next</span>
+      <Arrow direction="right" hasTail />
+    </Button>
+  </nav>
+);
 
-    this.prev = this.prev.bind(this);
-    this.next = this.next.bind(this);
-  }
-
-  prev() {
-    if (this.props.navigate && this.props.prev) {
-      this.props.navigate(this.props.prev);
-    }
-  }
-
-  next() {
-    if (this.props.navigate && this.props.next) {
-      this.props.navigate(this.props.next);
-    }
-  }
-
-  render({ isUnavailable }) {
-    return (
-      <nav className={styles.root} aria-hidden={isUnavailable ? 'true' : 'false'}>
-        <Button tabindex={isUnavailable ? -1 : 0} aria-controls="reader-stories" onClick={this.prev}>
-          <Arrow direction="left" hasTail />
-          <span>Previous</span>
-        </Button>
-        <Button tabindex={isUnavailable ? -1 : 0} aria-controls="reader-stories" onClick={this.next}>
-          <span>Next</span>
-          <Arrow direction="right" hasTail />
-        </Button>
-      </nav>
-    );
-  }
-}
-
-module.exports = RingNav;
+module.exports.displayName = 'RingNav';
