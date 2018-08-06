bd37b93269498aadf57d1106666da94f9ee7c126
Colin Gourlay <Gourlay.Colin@abc.net.au>
2018-08-06 11:33:53 +1000

    Added swipe navigation.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -21,6 +21,9 @@ class App extends Component {
 
     this.navigate = this.navigate.bind(this);
     this.saveAudioRef = this.saveAudioRef.bind(this);
+    this.swipeBegin = this.swipeBegin.bind(this);
+    this.swipeContinue = this.swipeContinue.bind(this);
+    this.swipeFinish = this.swipeFinish.bind(this);
     this.start = this.start.bind(this);
 
     this.updateRing(props.scene);
@@ -53,6 +56,50 @@ class App extends Component {
     this.audio = el;
   }
 
+  swipeBegin(event) {
+    if (this.swipe != null) {
+      return;
+    }
+
+    const { clientX, clientY } = event.touches ? event.touches[0] : event;
+
+    this.swipe = { x: clientX, y: clientY };
+  }
+
+  swipeContinue(event) {
+    if (typeof event.scale !== 'undefined' && event.scale !== 1) {
+      // Attempt to stop pinch-zoom
+      return event.preventDefault();
+    }
+
+    if (this.swipe == null) {
+      return;
+    }
+
+    const { clientX, clientY } = event.touches ? event.touches[0] : event;
+
+    this.swipe.dX = clientX - this.swipe.x;
+    this.swipe.dY = clientY - this.swipe.y;
+
+    if (Math.abs(this.swipe.dX) > 40 || Math.abs(this.swipe.dY) > 20) {
+      this.swipeFinish();
+    }
+  }
+
+  swipeFinish() {
+    if (this.swipe == null) {
+      return;
+    }
+
+    if (this.swipe.dX > 40 && this.state.prev) {
+      this.navigate(this.state.prev);
+    } else if (this.swipe.dX < -40 && this.state.next) {
+      this.navigate(this.state.next);
+    }
+
+    this.swipe = null;
+  }
+
   start() {
     this.setState({ hasStarted: true });
 
@@ -81,7 +128,18 @@ class App extends Component {
     const currentCreditsHTML = scene && scene.creditsHTML === current ? current : null;
 
     return (
-      <main role="main" className={styles.root}>
+      <main
+        role="main"
+        className={styles.root}
+        onMouseDown={current ? this.swipeBegin : null}
+        onTouchStart={current ? this.swipeBegin : null}
+        onMouseMove={current ? this.swipeContinue : null}
+        onTouchMove={current ? this.swipeContinue : null}
+        onMouseUp={current ? this.swipeFinish : null}
+        onTouchEnd={current ? this.swipeFinish : null}
+        onMouseLeave={current ? this.swipeFinish : null}
+        onTouchCancel={current ? this.swipeFinish : null}
+      >
         <Loader />
         {meta &&
           scene && (
