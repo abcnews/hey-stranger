3dbe005b4ee92b290026a4d6e31ab126998a8a3f
Colin Gourlay <Gourlay.Colin@abc.net.au>
2018-08-08 15:28:03 +1000

    Richtext is now a component that applies smartquotes

--- src/components/Richtext/index.js
+++ src/components/Richtext/index.js
@@ -1,6 +1,19 @@
-const { h } = require('preact');
+const { h, Component } = require('preact');
+const smartquotes = require('./smartquotes');
 const styles = require('./styles.css');
 
-module.exports = ({ html }) => <div className={styles.root} dangerouslySetInnerHTML={{ __html: html }} />;
+class Richtext extends Component {
+  componentDidMount() {
+    smartquotes(this.base);
+  }
 
-module.exports.displayName = 'Richtext';
+  componentDidUpdate() {
+    smartquotes(this.base);
+  }
+
+  render({ html }) {
+    return <div className={styles.root} dangerouslySetInnerHTML={{ __html: html }} />;
+  }
+}
+
+module.exports = Richtext;
