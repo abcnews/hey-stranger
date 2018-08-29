510c1764c39b9cfc3957874be2c30deb6ee881cc
Colin Gourlay <Gourlay.Colin@abc.net.au>
2018-08-29 10:51:08 +1000

    RingNav caches most revent prev/next names so they remain long enough to fade out when we return to the scene.

--- src/components/RingNav/index.js
+++ src/components/RingNav/index.js
@@ -4,18 +4,26 @@ const Button = require('../Button');
 const { withContext } = require('../AppContext');
 const styles = require('./styles.css');
 
-const RingNav = ({ isCurrentActor, next, prev, goTo }) => (
-  <nav className={styles.root} aria-hidden={isCurrentActor ? 'false' : 'true'}>
-    <Button tabindex={isCurrentActor ? 0 : -1} aria-controls="reader-stories" onClick={() => goTo(prev)}>
-      <Arrow direction="left" hasTail />
-      <span>{prev ? prev.name : ''}</span>
-    </Button>
-    <Button tabindex={isCurrentActor ? 0 : -1} aria-controls="reader-stories" onClick={() => goTo(next)}>
-      <span>{next ? next.name : ''}</span>
-      <Arrow direction="right" hasTail />
-    </Button>
-  </nav>
-);
+let formerNames = null;
+
+const RingNav = ({ isCurrentActor, next, prev, goTo }) => {
+  if (prev && next) {
+    formerNames = [prev.name, next.name];
+  }
+
+  return (
+    <nav className={styles.root} aria-hidden={isCurrentActor ? 'false' : 'true'}>
+      <Button tabindex={isCurrentActor ? 0 : -1} aria-controls="reader-stories" onClick={() => goTo(prev)}>
+        <Arrow direction="left" hasTail />
+        <span>{prev ? prev.name : formerNames ? formerNames[0] : ''}</span>
+      </Button>
+      <Button tabindex={isCurrentActor ? 0 : -1} aria-controls="reader-stories" onClick={() => goTo(next)}>
+        <span>{next ? next.name : formerNames ? formerNames[1] : ''}</span>
+        <Arrow direction="right" hasTail />
+      </Button>
+    </nav>
+  );
+};
 
 RingNav.displayName = 'RingNav';
 
