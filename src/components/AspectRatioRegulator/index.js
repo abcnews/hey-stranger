08f013d3243b367582669d818fd5af703cece2ba
Colin Gourlay <Gourlay.Colin@abc.net.au>
2018-08-30 13:41:28 +1000

    Experiment with staggering AspectRatioRegulator state updates so that orientation is checked after the timeout, but validity is checked imediately.

--- src/components/AspectRatioRegulator/index.js
+++ src/components/AspectRatioRegulator/index.js
@@ -18,26 +18,39 @@ class AspectRatioRegulator extends Component {
       this.minMQL.addListener(this.update);
     }
 
-    this.state = this.inferState();
+    this.state = {
+      ...this.inferOrientation(),
+      ...this.inferValidity()
+    };
   }
 
-  inferState() {
+  inferOrientation() {
     return {
-      isCounterClockwise: window.orientation === -90,
+      orientation: window.orientation
+    };
+  }
+
+  inferValidity() {
+    return {
       isMaxInvalid: this.maxMQL && this.maxMQL.matches,
       isMinInvalid: this.minMQL && this.minMQL.matches
     };
   }
 
   update() {
-    setTimeout(() => {
-      this.setState(this.inferState());
+    clearTimeout(this.inferOrientationTimeout);
+    this.inferOrientationTimeout = setTimeout(() => {
+      this.setState(this.inferOrientation());
     }, 0);
+    this.setState({
+      orientation: null,
+      ...this.inferValidity()
+    });
   }
 
   render() {
     const { children } = this.props;
-    const { isCounterClockwise, isMaxInvalid, isMinInvalid } = this.state;
+    const { isMaxInvalid, isMinInvalid, orientation } = this.state;
 
     return (
       <div
@@ -45,7 +58,7 @@ class AspectRatioRegulator extends Component {
           [styles.invalid]: isMinInvalid || isMaxInvalid,
           [styles.minInvalid]: isMinInvalid,
           [styles.maxInvalid]: isMaxInvalid,
-          [styles.isCounterClockwise]: isCounterClockwise
+          [styles.isCounterClockwise]: orientation === -90
         })}
       >
         <div className={styles.children}>{children}</div>
@@ -58,20 +71,22 @@ class AspectRatioRegulator extends Component {
               : null
           }
         >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 96 96">
-            <path
-              className={styles.device}
-              d="M30 13h36a2 2 0 0 1 2 2v66a2 2 0 0 1-2 2H30a2 2 0 0 1-2-2V15a2 2 0 0 1 2-2zm2 5v53h32V18H32z"
-            />
-            <path
-              className={styles.tick}
-              d="M43.822 52.058l-5.509-5.57-2.813 3.027 8.322 7.929L60.5 41.05 57.725 38z"
-            />
-            <path
-              className={styles.cross}
-              d="M48 45.074L55.074 38 58 40.926 50.926 48 58 55.074 55.074 58 48 50.926 40.926 58 38 55.074 45.074 48 38 40.926 40.926 38 48 45.074z"
-            />
-          </svg>
+          {orientation != null && (
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 96 96">
+              <path
+                className={styles.device}
+                d="M30 13h36a2 2 0 0 1 2 2v66a2 2 0 0 1-2 2H30a2 2 0 0 1-2-2V15a2 2 0 0 1 2-2zm2 5v53h32V18H32z"
+              />
+              <path
+                className={styles.tick}
+                d="M43.822 52.058l-5.509-5.57-2.813 3.027 8.322 7.929L60.5 41.05 57.725 38z"
+              />
+              <path
+                className={styles.cross}
+                d="M48 45.074L55.074 38 58 40.926 50.926 48 58 55.074 55.074 58 48 50.926 40.926 58 38 55.074 45.074 48 38 40.926 40.926 38 48 45.074z"
+              />
+            </svg>
+          )}
         </div>
       </div>
     );
