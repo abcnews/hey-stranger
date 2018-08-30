988771ae00b6b9f6216f73a22f5833468a494123
Colin Gourlay <Gourlay.Colin@abc.net.au>
2018-08-30 13:56:14 +1000

    Bit of AspectRatioRegulator refactoring

--- src/components/AspectRatioRegulator/index.js
+++ src/components/AspectRatioRegulator/index.js
@@ -6,6 +6,7 @@ class AspectRatioRegulator extends Component {
   constructor(props) {
     super(props);
 
+    this.inferState = this.inferState.bind(this);
     this.update = this.update.bind(this);
 
     if (this.props.max) {
@@ -18,47 +19,36 @@ class AspectRatioRegulator extends Component {
       this.minMQL.addListener(this.update);
     }
 
-    this.state = {
-      ...this.inferOrientation(),
-      ...this.inferValidity()
-    };
+    this.state = this.inferState();
   }
 
-  inferOrientation() {
+  inferState() {
     return {
-      orientation: window.orientation
-    };
-  }
-
-  inferValidity() {
-    return {
+      isCounterClockwise: window.orientation === -90,
       isMaxInvalid: this.maxMQL && this.maxMQL.matches,
       isMinInvalid: this.minMQL && this.minMQL.matches
     };
   }
 
   update() {
-    clearTimeout(this.inferOrientationTimeout);
-    this.inferOrientationTimeout = setTimeout(() => {
-      this.setState(this.inferOrientation());
+    clearTimeout(this.updateTimeout);
+    this.updateTimeout = setTimeout(() => {
+      this.setState(this.inferState);
     }, 0);
-    this.setState({
-      orientation: null,
-      ...this.inferValidity()
-    });
   }
 
   render() {
     const { children } = this.props;
-    const { isMaxInvalid, isMinInvalid, orientation } = this.state;
+    const { isCounterClockwise, isMaxInvalid, isMinInvalid } = this.state;
+    const isInvalid = isMaxInvalid || isMinInvalid;
 
     return (
       <div
         className={cn(styles.root, {
-          [styles.invalid]: isMinInvalid || isMaxInvalid,
-          [styles.minInvalid]: isMinInvalid,
+          [styles.counterClockwise]: isCounterClockwise,
+          [styles.invalid]: isInvalid,
           [styles.maxInvalid]: isMaxInvalid,
-          [styles.isCounterClockwise]: orientation === -90
+          [styles.minInvalid]: isMinInvalid
         })}
       >
         <div className={styles.children}>{children}</div>
@@ -66,27 +56,25 @@ class AspectRatioRegulator extends Component {
           className={styles.hint}
           role="alert"
           aria-label={
-            isMinInvalid || isMaxInvalid
+            isInvalid
               ? `Your aspect ratio is too ${isMinInvalid ? 'narrow' : 'wide'}. Please rotate your device.`
               : null
           }
         >
-          {orientation != null && (
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 96 96">
-              <path
-                className={styles.device}
-                d="M30 13h36a2 2 0 0 1 2 2v66a2 2 0 0 1-2 2H30a2 2 0 0 1-2-2V15a2 2 0 0 1 2-2zm2 5v53h32V18H32z"
-              />
-              <path
-                className={styles.tick}
-                d="M43.822 52.058l-5.509-5.57-2.813 3.027 8.322 7.929L60.5 41.05 57.725 38z"
-              />
-              <path
-                className={styles.cross}
-                d="M48 45.074L55.074 38 58 40.926 50.926 48 58 55.074 55.074 58 48 50.926 40.926 58 38 55.074 45.074 48 38 40.926 40.926 38 48 45.074z"
-              />
-            </svg>
-          )}
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 96 96">
+            <path
+              className={styles.device}
+              d="M30 13h36a2 2 0 0 1 2 2v66a2 2 0 0 1-2 2H30a2 2 0 0 1-2-2V15a2 2 0 0 1 2-2zm2 5v53h32V18H32z"
+            />
+            <path
+              className={styles.tick}
+              d="M43.822 52.058l-5.509-5.57-2.813 3.027 8.322 7.929L60.5 41.05 57.725 38z"
+            />
+            <path
+              className={styles.cross}
+              d="M48 45.074L55.074 38 58 40.926 50.926 48 58 55.074 55.074 58 48 50.926 40.926 58 38 55.074 45.074 48 38 40.926 40.926 38 48 45.074z"
+            />
+          </svg>
         </div>
       </div>
     );
